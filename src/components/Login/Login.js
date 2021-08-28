3e965f006a42ad20cacf3d48de8cd5d134e677f8
hasanfuad <hasanmdfuad@hotmail.com>
2021-08-28 18:01:04 +0600

    login page design commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Login.css'
+
 const Login = () => {
     const handleSubmitForm = (e) => {
             console.log(e.target.name, e.target.value);
@@ -9,15 +11,16 @@ const Login = () => {
         marginTop: "200px"
     }
     return (
-        <div className="d-flex justify-content-around "style={styles}>
+        <div className="d-flex justify-content-around login-bg"style={styles}>
             <form onSubmit={handleSubmitForm}>
-                <input className="rounded" style={{width: "300px", padding: "10px"}} type="text" name="name" onBlur={handleSubmitForm} placeholder="Your name" />
+                <p style={{fontSize: "20px", fontWeight: "600"}}>Create an account</p>
+                <input className="input-btn rounded" type="text" name="name" onBlur={handleSubmitForm} placeholder="Your name" />
                 <br/>
-                <input className="rounded" style={{width: "300px", padding: "10px", marginTop: "15px"}} type="text" name="email" onBlur={handleSubmitForm} placeholder="Your email" required/>
+                <input className="input-btn rounded" type="text" name="email" onBlur={handleSubmitForm} placeholder="Your email" required/>
                 <br/>
-                <input className="rounded" style={{width: "300px", padding: "10px", marginTop: "15px"}} type="password" name="password" onBlur={handleSubmitForm} placeholder="Your password" required/>
+                <input className="input-btn rounded" type="password" name="password" onBlur={handleSubmitForm} placeholder="Your password" required/>
                 <br/>
-                <input style={{width: "300px", padding: "10px", marginTop: "15px"}} type="submit"/>
+                <input className="input-btn rounded btn-warning form-btn" type="submit" value="Sign In"/>
             </form>
         </div>
     );
