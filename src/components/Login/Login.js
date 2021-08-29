bc078ec82e673edf92d6ecd60c847ba603f007be
hasanfuad <hasanmdfuad@hotmail.com>
2021-08-29 18:04:24 +0600

    login validation commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,46 +1,197 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-import './Login.css'
+import "./Login.css";
+
+import * as firebase from "firebase";
+import "firebase/auth";
+
+import firebaseConfig from "./firebaseConfig";
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Login = () => {
-    const handleSubmitForm = (e) => {
-            console.log(e.target.name, e.target.value);
-            let isValid = true;
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isLoggedIn: false,
+    name: "",
+    email: "",
+    success: "",
+    error: "",
+    password: "",
+  });
 
-            if(e.target.name === 'email'){
-                const validMail = /\S+@\S+\.\S+/.test(e.target.value);
-                isValid = validMail;
-            }
+  // Google validation
 
-            if(e.target.name === 'password'){
-                const validLength = e.target.value.length > 6;
-                const validPass = /\d{1}/.test(e.target.value);
-                
-                isValid = validLength && validPass;
-            }
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const googleBtn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
 
-            if(isValid){
-                console.log("Successfully logged in");
-            }
+  // Email validation
+  const handleInputField = (e) => {
+    let isValid = true;
+
+    if (e.target.name === "email") {
+      const validMail = /\S+@\S+\.\S+/.test(e.target.value);
+      isValid = validMail;
     }
 
-    const styles = {
-        marginTop: "200px"
+    if (e.target.name === "password") {
+      const validLength = e.target.value.length > 6;
+      const validPass = /\d{1}/.test(e.target.value);
+
+      isValid = validLength && validPass;
     }
-    return (
-        <div className="d-flex justify-content-around login-bg"style={styles}>
-            <form onSubmit={handleSubmitForm}>
-                <p style={{fontSize: "20px", fontWeight: "600"}}>Create an account</p>
-                <input className="input-btn rounded" type="text" name="name" onBlur={handleSubmitForm} placeholder="Your name" />
-                <br/>
-                <input className="input-btn rounded" type="text" name="email" onBlur={handleSubmitForm} placeholder="Your email" required/>
-                <br/>
-                <input className="input-btn rounded" type="password" name="password" onBlur={handleSubmitForm} placeholder="Your password" required/>
-                <br/>
-                <input className="input-btn rounded btn-warning form-btn" type="submit" value="Sign In"/>
-            </form>
-        </div>
-    );
+
+    if (isValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  };
+
+  const handleSubmitForm = (e) => {
+    if (newUser && user.email && user.password) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(user.email, user.password)
+        .then((userCredential) => {
+          const newUserInfo = { ...user };
+          newUserInfo.success = "New user successfully created.";
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          console.log(newUserInfo);
+        });
+    }
+
+    if (!newUser && user.email && user.password) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((userCredential) => {
+          const newUserInfo = { ...user };
+          newUserInfo.success = "New user successfully login!";
+          setUser(newUserInfo);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          setUser(newUserInfo);
+          updateUserName(user.name);
+        });
+    }
+
+    e.preventDefault();
+  };
+
+  const updateUserName = (name) => {
+    const user = firebase.auth().currentUser;
+
+    user
+      .updateProfile({
+        displayName: name,
+      })
+      .then((res) => {
+        console.log("Successfully updated info", res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const styles = {
+    marginTop: "200px",
+  };
+  return (
+    <div>
+      {user.success ? (
+        <p style={{backgroundColor: "gray", color: "green", textAlign: "center"}}>{user.success}</p>
+      ) : (
+        <p style={{ color: "red", textAlign: "center"}}>{user.error}</p>
+      )}
+      <div className="d-flex justify-content-around login-bg" style={styles}>
+        {user.isLoggedIn && (
+          <div>
+            <h2>Welcome, {user.name}</h2>
+            <p>Email: {user.email}</p>
+          </div>
+        )}
+
+        <form onSubmit={handleSubmitForm}>
+          <p
+            style={{ fontSize: "20px", fontWeight: "600"}}
+          >
+            {newUser ? "Create an account" : "Login account"}
+          </p>
+          {newUser && (
+            <input
+              className="input-btn rounded"
+              type="text"
+              name="name"
+              onBlur={handleInputField}
+              placeholder="Your name"
+            />
+          )}
+          <br />
+          <input
+            className="input-btn rounded"
+            type="text"
+            name="email"
+            onBlur={handleInputField}
+            placeholder="Your email"
+            required
+          />
+          <br />
+          <input
+            className="input-btn rounded"
+            type="password"
+            name="password"
+            onBlur={handleInputField}
+            placeholder="Your password"
+            required
+          />
+          <br />
+          <input
+            className="input-btn rounded btn-warning form-btn"
+            type="submit"
+            value={newUser ? "Sign Up" : "Sign In"}
+          />
+          <p
+            style={{
+              color: "orange",
+              textDecoration: "underline",
+              fontWeight: "600",
+              fontSize: "20px",
+              cursor: "pointer",
+            }}
+            className="mt-4"
+            onClick={() => setNewUser(!newUser)}
+          >
+            {!newUser ? "Create an account" : "Already an account ?"}
+          </p>
+          <br />
+          <button onClick={googleBtn}>Sign in with google</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyChN2Wq-3LouCfxMMZampUP3Fmpan6mwJI",
+    authDomain: "assignment-9-city-rides.firebaseapp.com",
+    projectId: "assignment-9-city-rides",
+    storageBucket: "assignment-9-city-rides.appspot.com",
+    messagingSenderId: "500129864627",
+    appId: "1:500129864627:web:c5b7f6e282314cd6bc09c6"
+  };
+
+  export default firebaseConfig;
